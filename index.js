6bd4092eed4d3860d30d0d1c73795ece7684d1ea
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2024-11-18 17:07:37 +0600

    updated to set cors

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const corsOptions = {
+  origin: ["https://algoarena-930d5.web.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  // credentials: true, // Allow cookies if needed
+};
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Database Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z5rmhar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
